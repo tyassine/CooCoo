33c477fe8ddd2cc2b37cc07946652d1f009d4408
Perrine Letellier <arkzyna@gmail.com>
2012-06-04 17:54:08 +0200

    slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,16 +5,18 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     ui->setupUi(this);
     this->setWindowTitle("CooCoo");
     this->setWindowIcon(QIcon(QString("E:/Dropbox/LO21/CooCoo/CooCoo.png")));
-    QObject::connect(ui->b0, SIGNAL(clicked()), this, SLOT(slot0()));
-    QObject::connect(ui->b1, SIGNAL(clicked()), this, SLOT(slot1()));
-    QObject::connect(ui->b2, SIGNAL(clicked()), this, SLOT(slot2()));
-    QObject::connect(ui->b3, SIGNAL(clicked()), this, SLOT(slot3()));
-    QObject::connect(ui->b4, SIGNAL(clicked()), this, SLOT(slot4()));
-    QObject::connect(ui->b5, SIGNAL(clicked()), this, SLOT(slot5()));
-    QObject::connect(ui->b6, SIGNAL(clicked()), this, SLOT(slot6()));
-    QObject::connect(ui->b7, SIGNAL(clicked()), this, SLOT(slot7()));
-    QObject::connect(ui->b8, SIGNAL(clicked()), this, SLOT(slot8()));
-    QObject::connect(ui->b9, SIGNAL(clicked()), this, SLOT(slot9()));
+
+    // Connections slot/signaux des boutons du clavier
+    QObject::connect(ui->b0, SIGNAL(clicked()), this, SLOT(on_0()));
+    QObject::connect(ui->b1, SIGNAL(clicked()), this, SLOT(on_1()));
+    QObject::connect(ui->b2, SIGNAL(clicked()), this, SLOT(on_2()));
+    QObject::connect(ui->b3, SIGNAL(clicked()), this, SLOT(on_3()));
+    QObject::connect(ui->b4, SIGNAL(clicked()), this, SLOT(on_4()));
+    QObject::connect(ui->b5, SIGNAL(clicked()), this, SLOT(on_5()));
+    QObject::connect(ui->b6, SIGNAL(clicked()), this, SLOT(on_6()));
+    QObject::connect(ui->b7, SIGNAL(clicked()), this, SLOT(on_7()));
+    QObject::connect(ui->b8, SIGNAL(clicked()), this, SLOT(on_8()));
+    QObject::connect(ui->b9, SIGNAL(clicked()), this, SLOT(on_9()));
     QObject::connect(ui->bEsp, SIGNAL(clicked()), this, SLOT(on_espace()));
     QObject::connect(ui->bVirg, SIGNAL(clicked()), this, SLOT(on_virgule()));
     QObject::connect(ui->bCommit, SIGNAL(clicked()), this, SLOT(on_commit()));
@@ -23,10 +25,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     QObject::connect(ui->bMult, SIGNAL(clicked()), this, SLOT(on_multiplication()));
     QObject::connect(ui->bDiv, SIGNAL(clicked()), this, SLOT(on_division()));
     QObject::connect(ui->bC, SIGNAL(clicked()), this, SLOT(on_effacer()));
-    QObject::connect(ui->bSwap, SIGNAL(clicked()), this, SLOT(on_swap()));
-    QObject::connect(ui->bSum, SIGNAL(clicked()), this, SLOT(on_sum()));
-    QObject::connect(ui->bDup, SIGNAL(clicked()), this, SLOT(on_dup()));
-    QObject::connect(ui->cClavier, SIGNAL(toggled(bool)), this, SLOT(on_clavier(bool)));
+    QObject::connect(ui->bParD, SIGNAL(clicked()), this, SLOT(on_parenthese_droite()));
+    QObject::connect(ui->bParG, SIGNAL(clicked()), this, SLOT(on_parenthese_gauche()));
+
     ui->b0->setShortcut(Qt::Key_0);
     ui->b1->setShortcut(Qt::Key_1);
     ui->b2->setShortcut(Qt::Key_2);
@@ -38,65 +39,68 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     ui->b8->setShortcut(Qt::Key_8);
     ui->b9->setShortcut(Qt::Key_9);
 
+    // Connections slot/signaux des boutons des fonctions de la pile
+    QObject::connect(ui->bSwap, SIGNAL(clicked()), this, SLOT(on_swap()));
+    QObject::connect(ui->bSum, SIGNAL(clicked()), this, SLOT(on_sum()));
+    QObject::connect(ui->bDup, SIGNAL(clicked()), this, SLOT(on_dup()));
 
+    // Connections slot/signaux des boutons des paramètres
+    QObject::connect(ui->cClavier, SIGNAL(toggled(bool)), this, SLOT(on_clavier(bool)));
+
+    // Connections slot/signaux des boutons des fonctions
+    QObject::connect(ui->bQuote, SIGNAL(clicked()), this, SLOT(on_quote()));
+    QObject::connect(ui->bCos, SIGNAL(clicked()), this, SLOT(on_cos()));
+    QObject::connect(ui->bSin, SIGNAL(clicked()), this, SLOT(on_sin()));
+    QObject::connect(ui->bTan, SIGNAL(clicked()), this, SLOT(on_tan()));
+    QObject::connect(ui->bCosh, SIGNAL(clicked()), this, SLOT(on_cosh()));
+    QObject::connect(ui->bSinh, SIGNAL(clicked()), this, SLOT(on_sinh()));
+    QObject::connect(ui->bTanh, SIGNAL(clicked()), this, SLOT(on_tanh()));
 }
 
-void MainWindow::slot1(){
+
+// Slot clavier
+void MainWindow::on_1(){
     ui->Afficheur->setText(ui->Afficheur->text() + "1");
 }
-
-void MainWindow::slot2(){
+void MainWindow::on_2(){
     ui->Afficheur->setText(ui->Afficheur->text() + "2");
 }
-
-void MainWindow::slot3(){
+void MainWindow::on_3(){
     ui->Afficheur->setText(ui->Afficheur->text() + "3");
 }
-
-void MainWindow::slot4(){
+void MainWindow::on_4(){
     ui->Afficheur->setText(ui->Afficheur->text() + "4");
 }
-
-void MainWindow::slot5(){
+void MainWindow::on_5(){
     ui->Afficheur->setText(ui->Afficheur->text() + "5");
 }
-
-void MainWindow::slot6(){
+void MainWindow::on_6(){
     ui->Afficheur->setText(ui->Afficheur->text() + "6");
 }
-
-void MainWindow::slot7(){
+void MainWindow::on_7(){
     ui->Afficheur->setText(ui->Afficheur->text() + "7");
 }
-
-void MainWindow::slot8(){
+void MainWindow::on_8(){
     ui->Afficheur->setText(ui->Afficheur->text() + "8");
 }
-
-void MainWindow::slot9(){
+void MainWindow::on_9(){
     ui->Afficheur->setText(ui->Afficheur->text() + "9");
 }
-
-void MainWindow::slot0(){
+void MainWindow::on_0(){
     ui->Afficheur->setText(ui->Afficheur->text() + "0");
 }
-
 void MainWindow::on_virgule(){
     ui->Afficheur->setText(ui->Afficheur->text() + ".");
 }
-
 void MainWindow::on_espace(){
     ui->Afficheur->setText(ui->Afficheur->text() + " ");
 }
-
-
 void MainWindow::on_addition(){
     ui->Afficheur->setText(ui->Afficheur->text() + "+");
 }
 void MainWindow::on_soustraction(){
     ui->Afficheur->setText(ui->Afficheur->text() + "-");
 }
-
 void MainWindow::on_multiplication(){
     ui->Afficheur->setText(ui->Afficheur->text() + "x");
 }
@@ -106,6 +110,63 @@ void MainWindow::on_division(){
 void MainWindow::on_effacer(){
     ui->Afficheur->setText("");
 }
+void MainWindow::on_parenthese_gauche(){
+      ui->Afficheur->setText(ui->Afficheur->text()+"(");
+}
+void MainWindow::on_parenthese_droite(){
+      ui->Afficheur->setText(ui->Afficheur->text()+")");
+}
+
+// Slots fonctions
+void MainWindow::on_quote(){
+      ui->Afficheur->setText(ui->Afficheur->text()+"'");
+}
+void MainWindow::on_cos(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"cos");
+}
+void MainWindow::on_sin(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"sin");
+}
+void MainWindow::on_tan(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"tan");
+}
+void MainWindow::on_cosh(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"cosh");
+}
+void MainWindow::on_sinh(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"sinh");
+}
+void MainWindow::on_tanh(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"tanh");
+}
+void MainWindow::on_mod(){
+   ui->Afficheur->setText(ui->Afficheur->text()+"mod");
+}
+
+void MainWindow::on_ln(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"ln");
+ }
+void MainWindow::on_log(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"log");
+ }
+void MainWindow::on_inv(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"inv");
+ }
+void MainWindow::on_sqr(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"sqr");
+ }
+void MainWindow::on_sqrt(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"sqrt");
+ }
+void MainWindow::on_sign(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"sign");
+ }
+void MainWindow::on_pow(){
+    ui->Afficheur->setText(ui->Afficheur->text()+"pow");
+ }
+
+
+// Slots paramètres
 
 void MainWindow::on_clavier(bool checked){
     if (checked)
--- mainwindow.h
+++ mainwindow.h
@@ -16,16 +16,17 @@ public:
     ~MainWindow();
 
 private slots:
-    void slot1();
-    void slot2();
-    void slot3();
-    void slot4();
-    void slot5();
-    void slot6();
-    void slot7();
-    void slot8();
-    void slot9();
-    void slot0();
+
+    void on_0();
+    void on_1();
+    void on_2();
+    void on_3();
+    void on_4();
+    void on_5();
+    void on_6();
+    void on_7();
+    void on_8();
+    void on_9();
     void on_espace();
     void on_virgule();
     void on_commit();
@@ -34,11 +35,34 @@ private slots:
     void on_multiplication();
     void on_division();
     void on_effacer();
+    void on_parenthese_droite();
+    void on_parenthese_gauche();
+
+    // slotsdes fonctions de la pile
     void on_swap();
     void on_sum();
     void on_dup();
+
+    // slots des paramètres
     void on_clavier(bool);
-    
+
+    // slots des fonctions
+    void on_quote();
+    void on_cos();
+    void on_sin();
+    void on_tan();
+    void on_cosh();
+    void on_sinh();
+    void on_tanh();
+    void on_mod();
+    void on_ln();
+    void on_log();
+    void on_inv();
+    void on_sqr();
+    void on_sqrt();
+    void on_sign();
+    void on_pow();
+
 private:
     Ui::MainWindow *ui;
 };
