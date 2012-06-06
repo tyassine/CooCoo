32d5a0a763700204b5e3e56a6bf29bea1d22715d
Perrine Letellier <arkzyna@gmail.com>
2012-06-06 22:32:50 +0200

    commit gardien

--- gardien.h
+++ gardien.h
@@ -8,16 +8,11 @@
 
 /*!
 * \file gardien.h
-* \author Le Feurmou/Leleu
+* \author Letellier/Yassine
 */
 
 /*! \class gardien
 * \brief Classe representant un gardien
-*
-* La classe permet de sauvegarder les etats d'une pile, dans le but de pouvoir les restaurer
-* Partie du design pattern "Memento"
-*
-*
 */
 
 
@@ -29,38 +24,10 @@ class gardien
     QVector <Pile> tabPile;/*!< Vector de pile*/
 
 public:
-    /*!
-* \brief Constructeur
-*
-* Construit un gardien
-*
-*/
     gardien();
-
-    /*!
-* \brief Undo
-* Permet de restaurer l'etat precedent
-* \return Pile suivante
-*
-*/
     Pile* undo();
-
-
-
-
-    /*!
-* \brief Redo
-* Permet de restaurer l'etat suivant
-* \return Pile suivante
-*
-*/
     Pile* redo();
 
-    /*!
-* \brief addMemento
-* Permet d'ajouter un etat dans le vecteur de pile
-* \param pile: pile a ajouter
-*/
     void addMemento(Pile pile);
 };
 
